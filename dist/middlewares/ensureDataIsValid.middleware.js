28eb6fa5ae7d5a25b90775e887b5d990c3e6cf76
julianairana <julianairana@hotmail.com>
2023-05-25 20:40:58 -0300

    feat/ remove build

--- dist/middlewares/ensureDataIsValid.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ensureDataIsValidMiddleware = void 0;
-const ensureDataIsValidMiddleware = (schema) => (request, response, next) => {
-    const validatedData = schema.parse(request.body);
-    request.body = validatedData;
-    return next();
-};
-exports.ensureDataIsValidMiddleware = ensureDataIsValidMiddleware;
--- dist/routers/login.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loginRoutes = void 0;
-const express_1 = require("express");
-const login_controllers_1 = require("../controllers/login.controllers");
-const ensureDataIsValid_middleware_1 = require("../middlewares/ensureDataIsValid.middleware");
-const login_schemas_1 = require("../schemas/login.schemas");
-exports.loginRoutes = (0, express_1.Router)();
-exports.loginRoutes.post("", (0, ensureDataIsValid_middleware_1.ensureDataIsValidMiddleware)(login_schemas_1.createLoginSchema), login_controllers_1.createLoginController);
